4d612bd9b39c1c6083cf382338a88cab45722f22
saeed <saeedbabakan10@gmail.com>
2025-03-26 01:24:39 -0700

    add dashboard url path

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-# from .views import user_login
+from .views import dashboard
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', dashboard, name='dashboard'),
 ]
