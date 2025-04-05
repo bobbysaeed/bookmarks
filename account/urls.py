cefdd6370f37cd0a7efad24a851016d750848510
saeed <saeedbabakan10@gmail.com>
2025-04-05 05:35:02 -0700

    add pass change and changedone url path

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,7 @@ from .views import dashboard
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='Password_Change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('', dashboard, name='dashboard'),
 ]
