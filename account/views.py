50b6826637a7a47ada8657f80f62eeeff6549469
saeed <saeedbabakan10@gmail.com>
2025-04-07 12:06:31 -0700

    add register function to the view for registration

--- account/views.py
+++ account/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 def user_login(request):
@@ -34,3 +34,25 @@ def dashboard(request):
         'account/dashboard.html',
         {'section': 'dashboard'}
     )
+
+def register(request):
+    if request.method == 'POST':
+        user_formform = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password']
+                )
+            new_user.save()
+            return render(
+                request,
+                'account/register_done.html',
+                {'new_user': new_user}
+            )
+    else:
+        user_form = UserRegistrationForm()
+    return render(
+        request,
+        'account/register.html',
+        {'user_form': user_form}
+        )
